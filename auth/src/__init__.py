d298d21fe468036583b9bae0facc78f0bc2f792a
Madhurachanna <madhurachanna23@gmail.com>
2020-09-08 23:46:39 +0530

    Postgres

--- auth/src/__init__.py
+++ auth/src/__init__.py
@@ -1,14 +1,20 @@
 from flask import Flask
+from src.config import Config
+from flask_sqlalchemy import SQLAlchemy
+# app = Flask(__name__)
 
-app = Flask(__name__)
 
-
-def create_app():
+def create_app(config_class=Config):
     app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db = SQLAlchemy()
+    db.init_app(app)
+    with app.app_context():
+        db.create_all()
 
     from src.routes.signup import signup
     from src.middleware.error_handler import errors
-
     app.register_blueprint(errors)
     app.register_blueprint(signup)
 
